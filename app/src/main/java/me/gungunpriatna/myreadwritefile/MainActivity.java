97a290b752f6da2bbfe9aaa37ba6518ec852d292
Gun Gun Priatna <gungun002@ummi.ac.id>
2018-08-02 10:08:50 +0700

    menambahkan code dalam method onCreate

--- app/src/main/java/me/gungunpriatna/myreadwritefile/MainActivity.java
+++ app/src/main/java/me/gungunpriatna/myreadwritefile/MainActivity.java
@@ -1,13 +1,54 @@
 package me.gungunpriatna.myreadwritefile;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnSound;
+    SoundPool sp ;
+    int wav;
+    boolean spLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSound = findViewById(R.id.btn_soundpool);
+        btnSound.setOnClickListener(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            sp = new SoundPool.Builder()
+                    .setMaxStreams(10)
+                    .build();
+        } else {
+            sp = new SoundPool(10, AudioManager.STREAM_MUSIC, 1);
+        }
+
+        sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
+                spLoaded = true;
+            }
+        });
+        wav = sp.load(this, R.raw.clinking_glasses, 1);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_soundpool:
+                if (spLoaded){
+                    sp.play(wav, 1, 1, 0, 0, 1);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }

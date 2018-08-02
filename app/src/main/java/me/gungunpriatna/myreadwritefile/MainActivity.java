87e076f8f23c41d02899f10347bbf69624d2f6e5
Gun Gun Priatna <gungun002@ummi.ac.id>
2018-08-02 10:25:01 +0700

    add resource and class MediaService

--- app/src/main/java/me/gungunpriatna/myreadwritefile/MainActivity.java
+++ app/src/main/java/me/gungunpriatna/myreadwritefile/MainActivity.java
@@ -1,6 +1,8 @@
 package me.gungunpriatna.myreadwritefile;
 
+import android.content.Intent;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +11,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
     Button btnSound;
+    Button btnMedia;
+    Button btnMediaStop;
     SoundPool sp ;
     int wav;
     boolean spLoaded = false;
+
+    Intent it;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSound = findViewById(R.id.btn_soundpool);
+        btnSound = (Button)findViewById(R.id.btn_soundpool);
+        btnMedia = (Button)findViewById(R.id.btn_mediaplayer);
+        btnMediaStop = (Button)findViewById(R.id.btn_mediaplayer_stop);
         btnSound.setOnClickListener(this);
+        btnMedia.setOnClickListener(this);
+        btnMediaStop.setOnClickListener(this);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             sp = new SoundPool.Builder()
                     .setMaxStreams(10)
@@ -32,10 +42,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
+                //Log.d("Load","true");
                 spLoaded = true;
             }
         });
+
         wav = sp.load(this, R.raw.clinking_glasses, 1);
+
+        it = new Intent(this, MediaService.class);
+        it.setAction(MediaService.ACTION_CREATE);
+        it.setPackage(MediaService.ACTION_PACKAGE);
+        startService(it);
     }
 
     @Override
@@ -43,12 +60,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.btn_soundpool:
-                if (spLoaded){
+                if (spLoaded == true ){
                     sp.play(wav, 1, 1, 0, 0, 1);
                 }
                 break;
+            case R.id.btn_mediaplayer:
+                it.setAction(MediaService.ACTION_PLAY);
+                it.setPackage(MediaService.ACTION_PACKAGE);
+                startService(it);
+
+                break;
+            case R.id.btn_mediaplayer_stop:
+                it.setAction(MediaService.ACTION_STOP);
+                it.setPackage(MediaService.ACTION_PACKAGE);
+                startService(it);
+                break;
+
             default:
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(it);
     }
 }
